1ebefe9c65d41bb46de98f6712b90c1f4e3475a6
dev-hospa <dev.hospa@gmail.com>
2019-07-20 08:20:51 +0200

    update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,7 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
 
-THEME = "C:/Users/Terka/themes/blueidea"
+# THEME = "C:/Users/Terka/themes/blueidea"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
